d957aa93060b890b3420beb950664e20a3604394
GBKanev18 <GBKanev18@codingburgas.bg>
2023-02-06 13:41:35 +0200

    Initial commit

---
+++ src/Importer.java
@@ -0,0 +1,68 @@
+public class Importer {
+
+    private String name;
+    private String address  ;
+    private String phoneNumber;
+
+    public Importer(String name, String address, String phoneNumber){
+
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public Importer()
+    {
+        this.name = "Kunchou";
+        this.address = "Burgas, zh.k. Slaveykov bl. 52 et. 15 ap. 218";
+        this.phoneNumber = "0887745789";
+
+    }
+
+    public String getName(){
+
+        return name;
+
+    }
+
+    public void setName(String name){
+
+        this.name = name;
+
+    }
+
+    public String getAddress(){
+
+        return address;
+
+    }
+
+    public void setAddress(String address){
+
+        this.address = address;
+
+    }
+
+    public String getPhoneNumber(){
+
+        return phoneNumber;
+
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    @Override
+    public String toString(){
+
+        return "Provider {" +
+                "name='" + name + '\'' +
+                ", city='" + address + '\'' +
+                ", age=" + phoneNumber +
+                '}';
+    }
+}
